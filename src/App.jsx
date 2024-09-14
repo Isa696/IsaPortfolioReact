d1cbcf0e76f460250be36df49ad56df1d6fb5501
Isa696 <isaias92@hotmail.com.ar>
2024-09-14 19:05:52 -0300

    Add React Helmet - App.jsx - metas description , title & thumbnail

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Contact from "./Components/Contact/Contact"
 import MyProjects from "./Components/MyProjetcs/MyProjects"
 import SideBar from "./Components/SideBar/SideBar"
 import Techs from "./Components/Techs/Techs"
+import { Helmet } from "react-helmet"
 import "./app.css"
 
 function App() {
@@ -10,6 +11,12 @@ function App() {
   return (
     <>
       <div id="page-wraper">
+
+        <Helmet>
+          <title>Isaias Portfolio</title>
+          <meta name="og:description" content="Portafolio personal como desarrollador frontend." />
+          <meta name="og:thumbnail" content={`${import.meta.env.PUBLIC_URL}/assets/images/thumbnail.jpg`} />
+        </Helmet>
 
         <SideBar />
 
