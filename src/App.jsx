f8421d4c225445072fe68be8ea3eab82b35e4702
Isa696 <isaias92@hotmail.com.ar>
2024-09-14 22:07:39 -0300

    add react helemet async

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import { Helmet, HelmetProvider } from "react-helmet-async"
 import AboutMe from "./Components/AboutMe/AboutMe"
 import Contact from "./Components/Contact/Contact"
 import MyProjects from "./Components/MyProjetcs/MyProjects"
 import SideBar from "./Components/SideBar/SideBar"
 import Techs from "./Components/Techs/Techs"
 import "./app.css"
+import imgPreview from "./assets/images/thumbnail.png"
 
 function App() {
   return (
     <>
       <div id="page-wraper">
+        <HelmetProvider>
+          <Helmet>
+          <meta name="og:thumbnail" content={imgPreview} />
+          </Helmet>
 
         <SideBar />
 
@@ -21,6 +27,7 @@ function App() {
         <Contact />
 
 
+        </HelmetProvider>
       </div>
     </>
   )
