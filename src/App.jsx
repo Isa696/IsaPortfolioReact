7784109a042ba46d8d0d2df42d9a9cff12e9cf8f
Isa696 <isaias92@hotmail.com.ar>
2024-09-14 21:13:40 -0300

    Change react helmet to react helmet asycn

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,19 @@ import Contact from "./Components/Contact/Contact"
 import MyProjects from "./Components/MyProjetcs/MyProjects"
 import SideBar from "./Components/SideBar/SideBar"
 import Techs from "./Components/Techs/Techs"
-import { Helmet } from "react-helmet"
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import "./app.css"
 
 function App() {
   return (
     <>
       <div id="page-wraper">
+      <HelmetProvider>
 
         <Helmet>
           <title>Isaias Portfolio</title>
-          <meta name="og:description" content="Portafolio personal como desarrollador frontend." />
-          <meta name="og:thumbnail" content="https://i.imgur.com/kf8bimK.jpg" />
+          <meta name="description" content="Portafolio personal como desarrollador frontend." />
+          <meta name="og:thumbnail" content="https://i.imgur.com/VZX2mte.png" />
         </Helmet>
 
         <SideBar />
@@ -28,6 +29,7 @@ function App() {
         <Contact />
 
 
+      </HelmetProvider>
       </div>
     </>
   )
